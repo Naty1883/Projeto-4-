6213691ac24f44abd28c4cebfd3812de802bb151
Naty1883 <139017640+Naty1883@users.noreply.github.com>
2025-06-06 08:39:53 -0300

    .

--- js/script.js
+++ js/script.js
@@ -55,7 +55,26 @@ function respostaSelecionada(opcaoSelecionada) {
         mostraResultado();
         return;
     }
-    mostraPergunta();
+    mostraPergunta();import { aleatorio, nome } from './aleatorio.js';Add commentMore actions
+import { perguntas } from './perguntas.js';
+
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.quer…vas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + " ";
+    if (opcaoSelecionada.proxima !== undefined) {
+        atual = opcaoSelecionada.proxima;
 }
 
 function mostraResultado() {
